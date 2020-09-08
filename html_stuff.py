7584c1bfbf7d4d6087c29aedc547c2f0c7ed2c88
dustpancake <fergusbkr@gmail.com>
2020-09-08 21:01:47 +0200

    modified fill_template into a decorator

--- html_stuff.py
+++ html_stuff.py
@@ -1,17 +1,31 @@
-def fill_template(html_raw, key_values):
-    """ 
-    substitutes html_raw with the key_values dictionary conts 
-    """
 
-    for key,values in key_values:
 
-        #count all occurences of key
-        num_occurences = html_raw.count(key)
+def fill_template(func):
 
-        #replace all occurences
-        html_raw = html_raw.replace(key,value,num_occurences)
+    def _decorator(*args, **kwargs):
+        """ 
+        substitutes html_raw with the key_values dictionary conts 
 
-    return html_raw
+        takes dict with n pairs 
+        """
+
+        html_raw, key_values = func(*args, **kwargs)
+
+        for key,values in key_values:
+
+            #count all occurences of key
+            num_occurences = html_raw.count(key)
+
+            #replace all occurences
+            html_raw = html_raw.replace(
+                r"{{" + key + r"}}",
+                value,
+                num_occurences
+            )
+
+        return html_raw
+
+    return _decorator
 
 
 
