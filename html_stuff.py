b0c70b0a40ab106b72a88005df234a020a0532b1
Shellywell123 <benjamin@shellswell.com>
2020-09-08 19:39:40 +0100

    added that function you wanted

---
+++ html_stuff.py
@@ -0,0 +1,20 @@
+def fill_template(html_raw, key_values):
+    """ 
+    substitutes html_raw with the key_values dictionary conts 
+    """
+    takes dict with n pairs 
+
+    for key,values in key_values:
+
+        #count all occurences of key
+        num_occurences = html_raw.count(key)
+
+        #replace all occurences
+        html_raw = html_raw.replace(key,value,num_occurences)
+
+    return html_raw
+
+
+
+
+
